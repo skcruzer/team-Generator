8c9709b7ab90ce81bb193f0289fcfff2fdf1693e
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-03-16 21:45:22 -0700

    engineer, intern

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { finished } = require("stream");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -37,6 +38,108 @@ const render = require("./lib/htmlRenderer");
 // employee array; new employees will be pushed into this array 
 let employeeArray = []
 
+const finished = () => {
+  // calling the render function and passing it in an array
+  let myTeamHtml = render(employeeArray)
+}
+
+// function for user to add more team members or not
+const contQuest = () => {
+  inquirer.prompt ([{
+    type: 'list',
+    name: 'yesOrNo',
+    message: 'Do you want to add more team members?',
+    choices: ['Yes', 'No']
+  }])
+  .then(yesOrNo => {
+    // go back to engineerOrIntern call
+    if (yesOrNo.yesOrNo === 'yes') {
+      engineerOrIntern() 
+    } else {
+      finished()
+    }
+  })
+}
+
+// function to add either engineer or intern
+const engineerOrIntern = () => {
+  inquirer.prompt([
+    {
+      type: 'list',
+      name: 'engOrInt',
+      message: 'Do you want to add an engineer or an intern?',
+      choices: ['Engineer', 'Intern']
+    }
+  ])
+  .then(answer => {
+    console.log(answer)
+    if(answer.engOrInt === 'Engineer') {
+      // questions if adding an engineer to the team
+      inquirer.prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of the engineer?'
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'What is the id of the engineer?'
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is the email address of the engineer?'
+        },
+        {
+          type: 'input',
+          name: 'github',
+          message: 'What is the github username of the engineer?'
+        } 
+      ])
+      .then(engineer => {
+        console.log(engineer)
+        let newEngineer = new Engineer(engineer.name, engineer.id, engineer.email, engineer.github)
+        // logs the engineer in the array with the other members
+        employeeArray.push(newEngineer)
+        console.log(employeeArray)
+        contQuest()
+      })
+    } else if (answer.engOrInt === 'Intern') {
+      // questions if adding an intern to team
+      inquirer.prompt ([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'What is the name of the intern?'
+        },
+        {
+          type: 'input',
+          name: 'id',
+          message: 'What is the id of the intern?'
+        },
+        {
+          type: 'input',
+          name: 'email',
+          message: 'What is the email address of the intern?'
+        },
+        {
+          type: 'input',
+          name: 'school',
+          message: 'What does/did the intern attend?'
+        } 
+      ])
+      .then(intern => {
+        console.log(intern)
+        let newIntern = new Intern(intern.name, intern.id, intern.email, intern.school)
+        employeeArray.push(newIntern)
+        console.log(employeeArray)
+        contQuest()
+      })
+    }
+  })
+}
+
 // using inquirer to get information about the team members
 inquirer.prompt ([
   {
@@ -52,13 +155,13 @@ inquirer.prompt ([
   {
     type: 'input',
     name: 'email',
-    message: 'What is the email of the team manager?'
+    message: 'What is the email address of the team manager?'
   },
   {
     type: 'input',
     name: 'officeNumber',
     message: 'What is the office number of the team manager?'
-  },
+  }
 ])
 .then(manager => {
   console.log(manager)
@@ -66,4 +169,5 @@ inquirer.prompt ([
   // will log manager information as an array
   employeeArray.push(newManager)
   console.log(employeeArray)
+  engineerOrIntern()
 })
