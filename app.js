a8a90eebdaaeb23e2fd113113a70904cf7573c3b
skcruzer <96440269+skcruzer@users.noreply.github.com>
2022-03-17 22:44:45 -0700

    finished function

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const fse = require('fs-extra')
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -143,15 +142,11 @@ inquirer.prompt ([
   engineerOrIntern()
 })
 
-// function to generate HTML page file using file system  
-const finished = () => {
-  // calling render function and passing it through an array
-  let myTeamHtml = render(employeeArray)
-  fse.outputFile('./output/team.html', myTeamHtml)
-  .then(() => {
-    console.log('You have successfully built your team! Please check the output folder for your team.html')
-  })
-  .catch(err => {
-    console.error(err)
-  })
+// function to input user data into html generating team page
+function finished() {
+  // Create the output directory if the output path doesn't exist
+  if (!fs.existsSync(OUTPUT_DIR)) {
+    fs.mkdirSync(OUTPUT_DIR);
+  }
+  fs.writeFileSync(outputPath, render(employeeArray), 'utf-8');
 }
